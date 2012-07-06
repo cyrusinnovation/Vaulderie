c3853441a0e00507ae4d1f0dcf6e2403a4b19214
Aldric Giacomoni <trevoke@gmail.com>
2012-07-05 22:25:27 -0400

    Vaulderie for Android

---
+++ Vaulderie-Android/src/com/cyrusinnovation/vaulderie/Vaulderie.java
@@ -0,0 +1,20 @@
+package com.cyrusinnovation.vaulderie;
+
+import android.app.Activity;
+import android.os.Bundle;
+//import android.webkit.WebResourceResponse;
+import org.apache.cordova.*;
+import org.apache.cordova.DroidGap;
+
+
+public class Vaulderie extends DroidGap
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
