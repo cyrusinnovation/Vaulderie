21a8192683791d1dbaccef1d79bfd76265ec6c87
Aldric Giacomoni <trevoke@gmail.com>
2012-07-18 07:36:31 -0400

    Add baby BackButton project, try to make BackButton work as needed

--- Vaulderie-Android/assets/www/js/app.js
+++ Vaulderie-Android/assets/www/js/app.js
@@ -1,44 +1,54 @@
 Array.prototype.rotate = function( n ) {
-  this.unshift.apply( this, this.splice( n, this.length ) )
+  this.unshift.apply( this, this.splice( n, this.length ) );
   return this;
-}
+};
 
 V = {};
-V['name'] = 'homepage';
-V['home'] = '#' + V['name'];
+V.name = 'homepage';
+V.home = '#' + V.name;
 
 $(document).ready(function () {
 
-  $(document).on("pagebeforechange", function (e, data) {
-    if (typeof data.toPage === "string") {
-      if (data["options"].fromPage[0]["id"] === V['name'] && data.toPage.match(/#char-1$/)) {
-        drawFirstPage();
-      } else if (data["options"].fromPage[0]["id"] === "char-1") {
-        drawRemainingPages();
-      } else if (data.toPage.match(/#results-0$/)) {
-        calculate();
-      } else if (data.toPage.match(/index.html$/)) {
-        resetApp();
-      }
-    }
-  });
-
-  document.addEventListener("backbutton", function(e){
-    if($.mobile.activePage.is(V['home']) || $.mobile.activePage.html().match(/LARP/)){
-      e.preventDefault();
+  function onConfirmQuit(button) {
+    if(button == "1") {
       navigator.app.exitApp();
     }
-    else {
-      navigator.app.backHistory()
-    }
-  }, false);
-  function charCount() {
-    return parseInt($("#char-count").val(), 10);
   }
+
+  function onBackButton() {
+
+  navigator.notification.alert("YABA!");
+
+//    console.log("here I am");
+//    if( $.mobile.activePage.is('#homepage') || /LARP/.test($.mobile.activePage.html()) ){
+//      console.log("Hello babies");
+//      //e.preventDefault();
+//      navigator.notification.confirm('Do you want to quit?',
+//                                     onConfirmQuit,
+//                                     'Exit the app',
+//                                     'OK,cancel');
+//    }
+//    else {
+//      console.log("BACK WAS PRESSED");
+//      navigator.app.backHistory();
+//    }
+  }
+
+  function onDeviceReady() {
+    $(document).bind('pageshow', function(e, data) {
+      document.addEventListener('backbutton', onBackButton, false);
+    });
+  }
+
+  document.addEventListener("deviceready", onDeviceReady, false);
 
   function removeExistingDivs() {
     $('div[id^="char"]').remove();
     $('div[id^="results"]').remove();
+  }
+
+  function charCount() {
+    return parseInt($("#char-count").val(), 10);
   }
 
   function drawGrid(names) {
@@ -68,7 +78,7 @@ $(document).ready(function () {
     content = $("<div />").attr({ "data-role":"content" }).append(drawGrid([]));
     content.append("<a href='#char-2' data-role='button'>Next</a>");
     page.append(content);
-    $(V['home']).parent().append(page);
+    $(V.home).parent().append(page);
   }
 
   function drawRemainingPages() {
@@ -92,7 +102,7 @@ $(document).ready(function () {
         content.append("<a href='#char-" + (i+1) + "' data-role='button'>Next</a>");
       }
       page.append(content);
-      $(V['home']).parent().append(page);
+      $(V.home).parent().append(page);
       $("input[type='text']").attr({"disabled": "disabled"});
     }
   }
@@ -121,11 +131,11 @@ $(document).ready(function () {
       row = {};
       names = getNames(i);
       values = getValues(i);
-      row['name'] = names.shift();
-      row['poolValue'] = parseInt(values.shift(), 10);
-      row['ratings'] = {};
+      row.name = names.shift();
+      row.poolValue = parseInt(values.shift(), 10);
+      row.ratings = {};
       names.forEach(function(el, idx) {
-        row['ratings'][el] = parseInt(values[idx], 10);
+        row.ratings[el] = parseInt(values[idx], 10);
       });
 
       result.push(row);
@@ -148,5 +158,19 @@ $(document).ready(function () {
     var url = $.mobile.path.parseUrl(window.location.href);
     window.location.href = url.hrefNoHash;
   }
+
+  $(document).on("pagebeforechange", function (e, data) {
+    if (typeof data.toPage === "string") {
+      if (data["options"].fromPage[0]["id"] === V['name'] && data.toPage.match(/#char-1$/)) {
+        drawFirstPage();
+      } else if (data["options"].fromPage[0]["id"] === "char-1") {
+        drawRemainingPages();
+      } else if (data.toPage.match(/#results-0$/)) {
+        calculate();
+      } else if (data.toPage.match(/index.html$/)) {
+        resetApp();
+      }
+    }
+  });
 
 });
