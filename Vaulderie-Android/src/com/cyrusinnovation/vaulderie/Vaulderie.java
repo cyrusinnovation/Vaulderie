84052706aa04ebd7c1f946f06aa1ca87a5fdceee
Aldric Giacomoni <trevoke@gmail.com>
2012-08-02 15:08:04 -0400

    [ANDROID] clean up Vaulderie.java

--- Vaulderie-Android/src/com/cyrusinnovation/vaulderie/Vaulderie.java
+++ Vaulderie-Android/src/com/cyrusinnovation/vaulderie/Vaulderie.java
@@ -1,11 +1,7 @@
 package com.cyrusinnovation.vaulderie;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.webkit.WebResourceResponse;
 import org.apache.cordova.*;
-import org.apache.cordova.DroidGap;
-
 
 public class Vaulderie extends DroidGap
 {
@@ -14,7 +10,6 @@ public class Vaulderie extends DroidGap
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main);
         super.loadUrl("file:///android_asset/www/index.html");
     }
 }
